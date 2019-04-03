e9c256c3a64fa206223bc25695060b5228c5181d
Hasib Ahmed <a.hasib@cern.ch>
2019-04-03 16:50:03 +0100

    calculates the difference between the sum of the diagonals of a square matrix

---
+++ algorithms/diag_diff.py
@@ -0,0 +1,24 @@
+#!/bin/python3
+import unittest
+
+# Complete the diagonalDifference function below.
+
+
+def diagonalDifference(arr):
+    sum_diag1 = 0
+    sum_diag2 = 0
+
+    j = len(arr) - 1
+    for i in range(len(arr)):
+        sum_diag1 += arr[i][i]
+        sum_diag2 += arr[i][j]
+        j -= 1
+    return abs(sum_diag1 - sum_diag2)
+
+
+if __name__ == '__main__':
+    arr = [[11, 2, 4], [4, 5, 6], [10, 8, -12]]
+    result = 15
+
+    tc = unittest.TestCase('__init__')
+    tc.assertEqual(diagonalDifference(arr), result)
