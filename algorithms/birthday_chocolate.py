648b59f8162fa2cc96a5d314e3a1b18b3d12f58e
Hasib Ahmed <a.hasib@cern.ch>
2019-04-10 21:41:36 +0100

    both brute force O(n^2) and O(n) solution

---
+++ algorithms/birthday_chocolate.py
@@ -0,0 +1,53 @@
+#!/bin/python3
+
+import unittest
+
+# brute force
+
+
+def birthday_brute(s, d, m):
+    ncomb = 0
+    for i in range(len(s)+1 - m):
+        sum = s[i]
+        for j in range(i+1, i+m):
+            sum += s[j]
+        if sum == d:
+            ncomb += 1
+    return ncomb
+
+# O(n) solution
+
+
+def birthday(s, d, m):
+    sub_array = []
+    sum = 0
+    ncomb = 0
+
+    for count, ele in enumerate(s):
+        # append to sub_arry and sum
+        # till elements in sub_array equals m
+        if (count < m):
+            sub_array.append(ele)
+            sum += ele
+        # once done check if the sum equals d
+        if count == m and sum == d:
+            ncomb += 1
+        # now reset sum to the sum of last m-1 elements
+        # and add the current element
+        if count >= m and count < len(s)+1-m:
+            sum = sum - sub_array.pop(0)  # O(1)
+            sum += ele
+            if sum == d:
+                ncomb += 1
+    return ncomb
+
+
+if __name__ == '__main__':
+    s = [1, 2, 1, 3, 2]
+    d = 3
+    m = 2
+
+    result = birthday(s, d, m)
+    # print(result)
+    t = unittest.TestCase('__init__')
+    t.assertEqual(birthday(s, d, m), 2)
